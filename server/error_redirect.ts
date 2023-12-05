d8eed83821c33eac7b803f81e1eaab36ac5ac576
rei <reimakesgames.dev@gmail.com>
2023-12-05 19:38:07 +0800

    Refactor errorRedirect function to use environment variables for client URL

--- server/error_redirect.ts
+++ server/error_redirect.ts
@@ -1,10 +1,7 @@
 import { type Response } from "express"
 
 export function errorRedirect(res: Response, status: number) {
-	const BASE_URL = process.env.BASE_URL || "http://localhost:5500"
-	const SUBDIRECTORY = process.env.SUBDIRECTORY || "client"
-
-	const CLIENT_URL = `${BASE_URL}/${SUBDIRECTORY}`
-
-	res.status(status).redirect(`${CLIENT_URL}/error.html?status=${status}`)
+	res.status(status).redirect(
+		`${process.env.CLIENT_URL}${process.env.CLIENT_SUBDIRECTORY}/error.html?status=${status}`
+	)
 }
